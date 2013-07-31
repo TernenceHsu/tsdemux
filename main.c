1f2ab9b6bfd201c1ed3f38ab247e7aef14272fbf
xubbwd <xubbwd@gmail.com>
2013-07-31 12:16:19 +0000

    change main.c

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
-﻿
+﻿/*
+
+  Copyright (C) 2013 xubinbin 徐彬彬 (Beijing China)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  
+  filename: main.c 
+  version : v1.0.0
+  time    : 2013/07/31 16:37 
+  author  : xubinbin ( xubbwd@gmail.com )
+  code URL: http://code.google.com/p/tsdemux/
+
+*/
 
 #include "fa_parseopt.h"
 #include "fa_ts2es.h"
-
 
 int main(int argc,char * argv[])
 {
